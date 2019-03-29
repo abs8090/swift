1420a6c8fb6d4b7cde62b850719f2cd6b767aa68
abs8090 <km.5432@hotmail.com>
2019-03-29 16:10:09 +0300

    day 6, clousers

--- Day6.playground/Contents.swift
+++ Day6.playground/Contents.swift
@@ -1,5 +1,14 @@
 import UIKit
 
+// basic clouser
 let driving = {
     print("I'm driving my Elentra")
 }
+driving()
+
+// clousers with parameters, parameters are listed in () that are inside the {}
+let traveling = {(place: String) in
+    print("Im going to \(place) in my car")
+}
+
+traveling("New York")
