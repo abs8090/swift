1ad6bd9bfc09e1dbba74b27a05f00f2b9310cc98
abs8090 <km.5432@hotmail.com>
2019-03-30 18:07:44 +0300

    day 6, clousers

--- Day6.playground/Contents.swift
+++ Day6.playground/Contents.swift
@@ -12,3 +12,33 @@ let traveling = {(place: String) in
 }
 
 traveling("New York")
+
+// clousers that returns value
+
+let carModel = {(model: String) -> String in
+    print("I'm driving:")
+    return model
+}
+
+print(carModel("Mercedes"))
+
+// clousers as parameters
+func travel(action:() -> Void){
+    print("i'm getting ready to go.")
+    action()
+    print("I arrived")
+}
+travel(action: driving)
+
+// trailing clouser syntax
+// we can define the clouser after the function
+
+func travel2(action:() -> Void){
+    print("I'm traveling again")
+    action()
+    print("hope it will be as fun as the last time!!")
+}
+
+travel2() {
+    print("this time we're going to Barcelona")
+}
